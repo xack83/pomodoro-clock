1ac29f69f1e9b45ac5b752064bfcdc120d1a6eaf
xack83 <xack83@gmail.com>
2015-10-17 16:52:00 +0200

    update 20151017

--- js/app.js
+++ js/app.js
@@ -1,5 +1,38 @@
 //Get user's location
 
 $(document).ready(function() {
-	
+    var sessionLgth = 1;
+    var breakLgth = 1;
+    var sessionSec = 60 * sessionLgth;
+    var breakSec = 60 * breakLgth;
+    display = $('#time');
+    $("#time").click(function() {
+        startTimer(sessionSec, breakSec, display); 
+    })
 });
+
+function startTimer(duration, next, display) {
+    var cycle = "session";
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0 && cycle === "session") {
+            cycle = "break";
+            timer = next;
+            startTimer(next, duration, display);
+        }
+        if (--timer < 0 && cycle === "break") {
+            cycle = "session";
+            timer = duration;
+            startTimer(duration, next, display);   
+        }
+    }, 1000);
+}
+
