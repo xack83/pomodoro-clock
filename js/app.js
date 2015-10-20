ee949e96c33d47c585cdde616ceae03e273e6f21
xack83 <xack83@gmail.com>
2015-10-20 16:40:34 +0200

    update 20151020

--- js/app.js
+++ js/app.js
@@ -1,38 +1,25 @@
-//Get user's location
-
+var min = 5;
+var time = min * 60;
+/*
 $(document).ready(function() {
-    var sessionLgth = 1;
-    var breakLgth = 1;
-    var sessionSec = 60 * sessionLgth;
-    var breakSec = 60 * breakLgth;
-    display = $('#time');
     $("#time").click(function() {
-        startTimer(sessionSec, breakSec, display); 
+        timer(time); 
     })
-});
+});*/
 
-function startTimer(duration, next, display) {
-    var cycle = "session";
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second 
+var minutes, seconds;
+function timer(duration, display) {
+  
+    duration--;
+    var seconds = Math.floor( (duration/1000) % 60 );
+    var minutes = Math.floor( (duration/1000/60) % 60 );
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.text(minutes + ":" + seconds);
-
-        if (--timer < 0 && cycle === "session") {
-            cycle = "break";
-            timer = next;
-            startTimer(next, duration, display);
-        }
-        if (--timer < 0 && cycle === "break") {
-            cycle = "session";
-            timer = duration;
-            startTimer(duration, next, display);   
-        }
-    }, 1000);
+    display.text(minutes + ":" + seconds);
+    if (duration <= 0) {
+        clearInterval(counter);
+        return;
+    }
 }
-
+display = $('#time');
+timer(time, display);
