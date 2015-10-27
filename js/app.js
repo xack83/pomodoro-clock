f18d82bb02d54ad3a0d486b5c6e736c1c1b4d5c7
xack83 <xack83@gmail.com>
2015-10-27 10:04:56 +0100

    update 20151027

--- js/app.js
+++ js/app.js
@@ -1,25 +1,51 @@
-var min = 5;
-var time = min * 60;
-/*
+var min = 2;
+//var time = min * 60;
+var time = 70;
+
 $(document).ready(function() {
     $("#time").click(function() {
-        timer(time); 
-    })
-});*/
+        timer(time, display); 
+    });
+    $("#reset").click(function() {
+        clearInterval(inter);
+        $("#time").text("25:00");
+    });
+});
 
-var counter=setInterval(timer, 1000); //1000 will  run it every 1 second 
 var minutes, seconds;
-function timer(duration, display) {
-  
-    duration--;
-    var seconds = Math.floor( (duration/1000) % 60 );
-    var minutes = Math.floor( (duration/1000/60) % 60 );
+display = $('#time');
 
-    display.text(minutes + ":" + seconds);
-    if (duration <= 0) {
-        clearInterval(counter);
-        return;
+function timer(duration, display) {
+
+    var inter = setInterval(function(){ 
+
+        var seconds = Math.floor(duration % 60);
+        var minutes = Math.floor( (duration/60) % 60 );
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        duration--;
+        if(duration<0){
+           clearInterval(inter);
+        }
+    }
+    , 1000);
+    
+}
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(timer, 1000);
+}
+/*
+var inter = setInterval(function(){ 
+    alert(time);
+    time--;
+    if(time<0){
+       clearInterval(inter);
     }
 }
-display = $('#time');
-timer(time, display);
+, 1000);*/
+
